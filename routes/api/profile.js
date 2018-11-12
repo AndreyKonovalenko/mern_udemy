0bc90f6070af969afca6377d31b6a31cdb63bba8
AndreyKonovalenko <andreinew@mail.ru>
2018-11-12 22:46:12 +0300

    Finished profile validation logic and fix bug in check if handle exists logic

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,6 +27,7 @@ router.get(
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+      .populate('user', ['name'])
       .then(profile => {
         if (!profile) {
           errors.noprofile = 'There is no profile for thes user';
@@ -46,7 +47,6 @@ router.post(
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const { errors, isValid } = validateProfileInput(req.body);
-
     // Chaeck Validation
     if (!isValid) {
       // Return any errors with 400 status
@@ -57,7 +57,7 @@ router.post(
     const profileFields = {};
     profileFields.user = req.user.id;
     if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.compony) profileFields.compony = req.body.compony;
+    if (req.body.company) profileFields.company = req.body.company;
     if (req.body.website) profileFields.website = req.body.website;
     if (req.body.bio) profileFields.bio = req.body.bio;
     if (req.body.ststus) profileFields.ststus = req.body.ststus;
@@ -65,14 +65,14 @@ router.post(
       profileFields.githubusername = req.body.githubusername;
     // Skills - Split into array
     if (typeof req.body.skills !== 'undefined') {
-      profileFields.skills = req.body.skils.split(',');
+      profileFields.skills = req.body.skills.split(',');
     }
 
     // Social
     profileFields.social = {};
     if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
     if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-    if (req.body.facebook) profileFields.social.facbook = req.body.facbook;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
     if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
     if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
@@ -83,8 +83,7 @@ router.post(
           { user: req.user.id },
           { $set: profileFields },
           { new: true }
-        );
-        then(profile => res.json(profile));
+        ).then(profile => res.json(profile));
       } else {
         // Create
 
@@ -93,10 +92,12 @@ router.post(
           if (profile) {
             errors.handle = 'That handle already exists';
             res.status(400).json(errors);
+          } else {
+            // Save Profile
+            new Profile(profileFields)
+              .save()
+              .then(profile => res.json(profile));
           }
-
-          // Save Profile
-          new Profile(profile).save().then(profile => res.json(profile));
         });
       }
     });

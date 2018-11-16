ee9dad0df10a2940018503541bc78ebe5b7b23e6
AndreyKonovalenko <andreinew@mail.ru>
2018-11-16 21:36:21 +0300

    Added draft ability to post experience as an array inside profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 
 // Load Input validaition
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
 
 // Load Profile Model
 const Profile = require('../../models/Profile');
@@ -156,6 +157,41 @@ router.post(
           }
         });
       }
+    });
+  }
+);
+
+// @router POST api/profile/experience
+// @decx   Add experience to profile
+// @access Privet
+
+router.post(
+  '/experience',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    console.log('request', req.body);
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Chaeck Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        discription: req.body.discription
+      };
+      // Add to exp array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
     });
   }
 );

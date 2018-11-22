75466da73f6420a63329ef5017e4781142398d58
Indrandeva <andreinew@mail.ru>
2018-11-22 09:48:39 +0000

    Added ability to delete post by id as a atherized user

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 // Post model
-
 const Post = require('../../models/Post');
+
+// Profile model 
+const Profile = require('../../models/Profile');
 
 // Validation 
 const validatePostInput = require('../../validation/post');
@@ -25,9 +27,8 @@ router.get('/', (req, res) => {
   Post.find()
     .sort({date: -1})
     .then(posts => res.json(posts))
-    .catch(err => res.status(404));
+    .catch(err => res.status(404).json({nopostsfound: 'No posts found'}));
 });
-
 
 // @router GET api/posts/:id
 // @decx   Get post by id
@@ -36,10 +37,8 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
     .then(post => res.json(post))
-    .catch(err => res.status(404));
+    .catch(err => res.status(404).json({nopostfound: 'No post found with that ID'}));
 });
-
-
 
 // @router POST api/posts
 // @decx   Create post
@@ -65,5 +64,28 @@ router.post('/', passport.authenticate('jwt', {session: false}),
     newPost.save().then(post => res.json(post));
   }
 );
+
+// @router DELETE api/posts/:id
+// @decx   DELETE post by id
+// @access Privete
+
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            // Check for post owner
+            if(post.user.toString() !== req.user.id) {
+              // 401 is unauthorised http request status
+              return res.status(401).json({notauthorized: 'User not authorized'});
+            }
+            
+            // Delete
+            post.remove().then(() => res.json({success: true}))
+          })
+          .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+      })
+});
+
 
 module.exports = router;

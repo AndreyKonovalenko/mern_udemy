589d235d53cfbba15966787c776383eac2246174
Indrandeva <andreinew@mail.ru>
2018-11-26 14:57:51 +0000

    Added Landign componenent and have issue with don not loading background img

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const footer = () => {
     return (
-        <footer className="bg-dark text-white mt-5 p-4 text-center">
-            Copyright &copy; 2018 Dev Connector
-        </footer>
+      <footer className="bg-dark text-white mt-5 p-4 text-center">
+        Copyright &copy; {new Date().getFullYear()} Dev Connector
+      </footer>
     );
 };
 

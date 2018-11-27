5a1656629c72178e3196561e39adb9b13ae1cd21
Indrandeva <andreinew@mail.ru>
2018-11-27 06:43:10 +0000

    Moved custom css file to pulic folder to prevent css-loader issue

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div>
         <Navbar />
         <Landing />
         <Footer />

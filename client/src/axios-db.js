1ec8a6e61c4fc33291dfa7527678275d402a7fd1
Indrandeva <andreinew@mail.ru>
2018-12-04 13:57:20 +0000

    added default db url to axios-db.js file and updated package.json concerinig this

--- client/src/axios-db.js
+++ client/src/axios-db.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 
 //const url = 'http://localhost:5000'; // this is for local development
 //const url = 'http://mern-bereon.c9users.io:8081'; // this is for C9 IDE development
-console.log(process.env.REACT_APP_URL);
+
+let url = 'http://localhost:5000';
+
+if(process.env.REACT_APP_URL === 'http://mern-bereon.c9users.io:8081') {
+    url = process.env.REACT_APP_URL;
+}
+
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_URL
+  baseURL: url
   
 });
 

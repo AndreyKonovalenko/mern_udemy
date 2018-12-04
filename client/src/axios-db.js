4b43cbf459408c232b2d07a8c59d65b44c4c61a0
Indrandeva <andreinew@mail.ru>
2018-12-04 13:46:55 +0000

    Setup two npm scripts config for running db on localhost machine and on cloud9 cloud machine

--- client/src/axios-db.js
+++ client/src/axios-db.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 //const url = 'http://localhost:5000'; // this is for local development
-const url = 'http://mern-bereon.c9users.io:8081'; // this is for C9 IDE development
-
+//const url = 'http://mern-bereon.c9users.io:8081'; // this is for C9 IDE development
+console.log(process.env.REACT_APP_URL);
 const instance = axios.create({
-  baseURL: url
+  baseURL: process.env.REACT_APP_URL
+  
 });
 
 export default instance;

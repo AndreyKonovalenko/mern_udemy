807d79655aa84fc74e8ceebd70a9a3e0f2dc619f
Indrandeva <andreinew@mail.ru>
2018-12-19 14:22:05 +0000

    Working on CreateProfile component

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
-import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+//import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import SelectListGroup from '../common/SelectListGroup';
-import InputGroup from '../common/InputGroup';
+//import InputGroup from '../common/InputGroup';
 
 class CreateProfile extends Component {
   state = {
@@ -25,13 +25,30 @@ class CreateProfile extends Component {
     errors: {}
   };
 
-  onSubmit = event => {
+  onSubmitHandler = event => {
     event.preventDefault();
     console.log('submit');
   };
+  
+  onChangeHandler = event => {
+    this.setState({[event.target.name]: event.target.value});
+  }
 
   render() {
     const { errors } = this.state;
+    
+    // Select options for status
+    const options = [
+      { label: '* Select Professipnal Status ', value: 0},
+      { label: 'Developer', value: 'Developer' },
+      { label: 'Junior Developer', value: 'Junior Developer' },
+      { label: 'Senior Developer', value: 'Senior Developer' },
+      { label: 'Manager', value: 'Manager' },
+      { label: 'Student or Learning', value: 'Student or Learning' },
+      { label: 'Insctuctor or Teacher', value: 'Insctuctor or Teacher' },
+      { label: 'Intern', value: 'Intern' },
+      { label: 'Other', value: 'Othen'}
+    ];
     return (
       <div className='create-profile'>
         <div className='container'>
@@ -42,14 +59,48 @@ class CreateProfile extends Component {
                 Let's get some information to make your profile stand out
               </p>
               <small className='d-block pb-3'>* - required fielsd</small>
-              <form onSubmit={this.onSubmit}>
+              <form onSubmit={this.onSubmitHandler}>
                 <TextFieldGroup
                   placeholder='* Profile Handle'
+                  value={this.state.handle}
                   nama={this.state.handle}
-                  onChange={this.onChange}
+                  onChange={this.onChangeHandler}
                   error={errors.handle}
                   info='A unique handle for you profile URL. Your full name, company name,
                   nickname'
+                />
+                <SelectListGroup
+                  placeholder='Status'
+                  value={this.state.status}
+                  nama='status'
+                  onChange={this.onChangeHandler}
+                  options={options}
+                  error={errors.status}
+                  info='Give us an idea where you are at in your career'
+                />
+                <TextFieldGroup
+                  placeholder='Company'
+                  name='company'
+                  value={this.state.company}
+                  onChange={this.onChangeHandler}
+                  error={errors.company}
+                  info='Gould be your own company or one you work for'
+                />
+                <TextFieldGroup
+                  placeholder='Website'
+                  name='website'
+                  value={this.state.website}
+                  onChange={this.onChangeHandler}
+                  error={errors.website}
+                  info='Could be your own website or a company one'
+                />
+                <TextFieldGroup
+                  placeholder='Location'
+                  name='location'
+                  value={this.state.location}
+                  onChange={this.onChangeHandler}
+                  error={errors.location}
+                  info='City or city & state suggested (eg. Boston, MA)'
                 />
               </form>
             </div>

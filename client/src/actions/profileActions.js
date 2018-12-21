6182209868a83cf58030011d1c476aa3bfa483c6
Indrandeva <andreinew@mail.ru>
2018-12-21 15:20:39 +0000

    Working on createProfile action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,7 +1,7 @@
 import axios from '../axios-db';
 //import setAuthToken from '../utils/setAuthToken';
 //import jwt_decode from 'jwt-decode';
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from './types';
 
 // Get current profile
 
@@ -19,6 +19,20 @@ export const getCurrentProfile = () => dispatch => {
       dispatch({
         type: GET_PROFILE,
         payload: {}
+      })
+    );
+};
+
+// Create Profile
+
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.respons.data
       })
     );
 };

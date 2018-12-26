236588f7ec499eb793a01daaf4abd84dd5b5042b
Indrandeva <andreinew@mail.ru>
2018-12-26 09:57:41 +0000

    Added new functional component ProfileActions

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
+import ProfileActions from './ProfileActions.js';
+
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
@@ -17,7 +19,15 @@ class Dashboard extends Component {
     } else {
       // Check if logged in user has profile data
       if (Object.keys(profile).length > 0) {
-        dashboardContent = <h4>TODO DISPLAY PROFILE</h4>;
+        dashboardContent = (
+          <div>
+             <p className='lead text-muted'>
+              Welcom <Link to={`/profile/${profile.handle}`}>
+              {user.name}</Link>
+             </p>
+             <ProfileActions />
+          </div>
+        );
       } else {
         // User is logged in but has no profiel
         dashboardContent = (

e274e9b1790b70541825575d00d367a7db5e72ae
Indrandeva <andreinew@mail.ru>
2018-12-29 10:50:21 +0000

    Have issue with option selection in CreateProfile.js

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -7,7 +7,7 @@ import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import SelectListGroup from '../common/SelectListGroup';
 import InputGroup from '../common/InputGroup';
 import { createProfile, getCurrentProfile } from '../../actions/profileActions';
-import {isEmpty} from '../../validation/is-empty';
+import isEmpty from '../../validation/is-empty';
 
 class EditProfile extends Component {
   state = {
@@ -30,6 +30,52 @@ class EditProfile extends Component {
 
   componentDidMount(){
     this.props.getCurrentProfile();
+    
+     if(this.props.profile.profile) {
+      const profile = this.props.profile.profile;
+      // Bring skills array back to CSV
+      const skillsCSV = profile.skills.join(',');
+      // IF  profile field doesn't exist, make empty string
+      profile.company = !isEmpty(profile.company) ? profile.company : '';
+      profile.website = !isEmpty(profile.website) ? profile.website: '';
+      profile.location = !isEmpty(profile.location) ? profile.location: '';
+      profile.githubusername = !isEmpty(profile.githubusername)
+        ? profile.githubusername
+        : '';
+      profile.bio = !isEmpty(profile.bio) ? profile.bio : '';
+      profile.social = !isEmpty(profile.social) ? profile.social : {};
+      profile.twitter = !isEmpty(profile.social.twitter) 
+        ? profile.social.twitter
+        : ''; 
+      profile.facebook = !isEmpty(profile.social.facebook)
+        ? profile.social.facebook
+        : '';
+      profile.linkedin = !isEmpty(profile.social.linkedin)
+        ? profile.social.linkedin
+        : '';
+      profile.youtube = !isEmpty(profile.social.youtube)
+        ? profile.social.youtube
+        : '';
+      profile.instagram = !isEmpty(profile.social.instagram)
+        ? profile.social.instagram
+        : '';
+        console.log(profile);
+        // Set comopnet fields state
+        this.setState({
+          handle: profile.handle,
+          company: profile.company,
+          website: profile.website,
+          location: profile.location,
+          skills: skillsCSV,
+          githubusername: profile.githubusername,
+          bio: profile.bio,
+          twitter: profile.twitter,
+          facebook: profile.facebook,
+          linkedin: profile.linkedin,
+          instagram: profile.instagram,
+          youtube: profile.youtube
+        })
+     }
   }
 
   onSubmitHandler = event => {
@@ -59,13 +105,7 @@ class EditProfile extends Component {
   render() {
     const { displaySocialInputs } = this.state;
     const formBasicClass = 'form-control form-control-lg';
-    console.log(this.props.profile.profile);
     
-    if(this.props.profile.profile) {
-      const profile = this.props.profile.profile;
-      // Bring skills array back to CSV
-      const skillsCSV = profile.skills.join(',');
-    }
     let socialInputs;
 
     if (displaySocialInputs) {

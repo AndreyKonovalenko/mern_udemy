993a31c791ee923b8aed67a95df288943fd5643c
AndreyKonovalenko <andreinew@mail.ru>
2019-01-05 19:47:16 +0300

    working on Experience component

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -2,14 +2,42 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import Moment from 'react-moment';
 
 class Experience extends Component {
   render() {
+    const experience = this.props.experience.map(exp => (
+      <tr key={exp._id}>
+        <td>{exp.company}</td>
+        <td>{exp.title}</td>
+        <td>
+          <Moment format='DD.MM.YYYY'>{exp.from}</Moment> -{' '}
+          {exp.to === null ? (
+            'Now'
+          ) : (
+            <Moment format='DD.MM.YYYY'>{exp.to}</Moment>
+          )}
+        </td>
+        <td>
+          <button className='btn btn-danger'>Delete</button>
+        </td>
+      </tr>
+    ));
     return (
       <div>
-        <h1>This is Draft of Experince component</h1>
+        <h4 className='mb-4'>Experience Credentials</h4>
+        <table className='table'>
+          <tr>
+            <th>Company</th>
+            <th>Title</th>
+            <th>Years</th>
+            <th />
+          </tr>
+          <tbody>{experience}</tbody>
+        </table>
       </div>
     );
   }
 }
-export default Experience;
+
+export default connect(null)(withRouter(Experience));

d33505a01d1bb744b824771b8fab767412f82784
AndreyKonovalenko <andreinew@mail.ru>
2019-01-09 20:07:21 +0300

    Added Profile action which is gitProfileByHandle

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,43 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 import ProfileHeader from './ProfileHeader';
 import ProfileAbout from './ProfileAbout';
 import ProfileCreds from './ProfileCreds';
 import ProfileGitHub from './ProfileGitHub';
-import Spinner from '../common/Spinner';
-//import {getProfileByHandle} from '../actions/ProfileActions';
-
+//import Spinner from '../common/Spinner';
+import { getProfileByHandle } from '../../actions/profileActions';
 
 class Profile extends Component {
-  
   componentDidMount() {
-    // if(this.props.match.params.handle) {
-    //   this.props.getProfileByHandle(this.props.match.params.handle);
-    // }
-    console.log(this.props.match.params);
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
+    }
+    console.log(typeof this.props.match.params.handle);
   }
-  render(){
-    
+  render() {
+    const { profile } = this.props.profile;
+    console.log(profile);
     return (
-    <div>
-      <ProfileHeader />
-      <ProfileAbout />
-      <ProfileCreds />
-      <ProfileGitHub />
-    </div>
+      <div>
+        <ProfileHeader />
+        {profile ? <h1>{profile.user.name}</h1> : null}
+        <ProfileAbout />
+        <ProfileCreds />
+        <ProfileGitHub />
+      </div>
     );
   }
 }
 
 Profile.propTyeps = {
-  profile: PropTypes.object.isRequired
-}
+  profile: PropTypes.object.isRequired,
+  getProfileByHandle: PropTypes.func.isRequired
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = state => ({
   profile: state.profile
-}
+});
 
-export default connect(mapStateToProps)(Profile);
+export default connect(
+  mapStateToProps,
+  { getProfileByHandle }
+)(Profile);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -28,20 +28,24 @@ router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
 // @decx   GET current users profile
 // @access Privet
 
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const errors = {};
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
 
-  Profile.findOne({ user: req.user.id })
-    .populate('user', ['name'])
-    .then(profile => {
-      if (!profile) {
-        errors.noprofile = 'There is no profile for this user';
-        return res.status(404).json(errors);
-      }
-      res.json(profile);
-    })
-    .catch(err => res.status(404).json(err));
-});
+    Profile.findOne({ user: req.user.id })
+      .populate('user', ['name'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 // @router GET api/profile/all
 // @decx   Get all profile
@@ -94,125 +98,144 @@ router.get('/user/:user_id', (req, res) => {
       }
       res.json(profile);
     })
-    .catch(err => res.status(404).json({ profile: 'There is no profile for this user' }));
+    .catch(err =>
+      res.status(404).json({ profile: 'There is no profile for this user' })
+    );
 });
 
 // @router POST api/profile
 // @decx   Create or Edit user profile
 // @access Privet
-router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const { errors, isValid } = validateProfileInput(req.body);
-  // Chaeck Validation
-  if (!isValid) {
-    // Return any errors with 400 status
-    return res.status(400).json(errors);
-  }
-
-  // Get fields
-  const profileFields = {};
-  profileFields.user = req.user.id;
-  if (req.body.handle) profileFields.handle = req.body.handle;
-  if (req.body.company) profileFields.company = req.body.company;
-  if (req.body.website) profileFields.website = req.body.website;
-  if (req.body.bio) profileFields.bio = req.body.bio;
-  if (req.body.location) profileFields.location = req.body.location;
-  if (req.body.status) profileFields.status = req.body.status;
-  if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
-  // Skills - Split into array
-  if (typeof req.body.skills !== 'undefined') {
-    profileFields.skills = req.body.skills.split(',');
-  }
-
-  // Social
-  profileFields.social = {};
-  if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-  if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-  if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-  if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-  if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
-
-  Profile.findOne({ user: req.user.id }).then(profile => {
-    if (profile) {
-      // Update
-      Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true }).then(
-        profile => res.json(profile)
-      );
-    } else {
-      // Create
-
-      // Check if handle exisits
-      Profile.findOne({ handle: profileFields.handle }).then(profile => {
-        if (profile) {
-          errors.handle = 'That handle already exists';
-          res.status(400).json(errors);
-        } else {
-          // Save Profile
-          new Profile(profileFields).save().then(profile => res.json(profile));
-        }
-      });
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+    // Chaeck Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
     }
-  });
-});
+
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    // Skills - Split into array
+    if (typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.skills.split(',');
+    }
+
+    // Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Create
+
+        // Check if handle exisits
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = 'That handle already exists';
+            res.status(400).json(errors);
+          } else {
+            // Save Profile
+            new Profile(profileFields)
+              .save()
+              .then(profile => res.json(profile));
+          }
+        });
+      }
+    });
+  }
+);
 
 // @router POST api/profile/experience
 // @decx   Add experience to profile
 // @access Privet
 
-router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const { errors, isValid } = validateExperienceInput(req.body);
+router.post(
+  '/experience',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
 
-  // Chaeck Validation
-  if (!isValid) {
-    // Return any errors with 400 status
-    return res.status(400).json(errors);
+    // Chaeck Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+      // Add to exp array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
+    });
   }
-
-  Profile.findOne({ user: req.user.id }).then(profile => {
-    const newExp = {
-      title: req.body.title,
-      company: req.body.company,
-      location: req.body.location,
-      from: req.body.from,
-      to: req.body.to,
-      current: req.body.current,
-      description: req.body.description
-    };
-    // Add to exp array
-    profile.experience.unshift(newExp);
-
-    profile.save().then(profile => res.json(profile));
-  });
-});
+);
 
 // @router POST api/profile/education
 // @decx   Add education to profile
 // @access Privet
 
-router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const { errors, isValid } = validateEducationInput(req.body);
+router.post(
+  '/education',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
 
-  // Chaeck Validation
-  if (!isValid) {
-    // Return any errors with 400 status
-    return res.status(400).json(errors);
+    // Chaeck Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+      // Add to exp array
+      profile.education.unshift(newEdu);
+
+      profile.save().then(profile => res.json(profile));
+    });
   }
-
-  Profile.findOne({ user: req.user.id }).then(profile => {
-    const newEdu = {
-      school: req.body.school,
-      degree: req.body.degree,
-      fieldofstudy: req.body.fieldofstudy,
-      from: req.body.from,
-      to: req.body.to,
-      current: req.body.current,
-      description: req.body.description
-    };
-    // Add to exp array
-    profile.education.unshift(newEdu);
-
-    profile.save().then(profile => res.json(profile));
-  });
-});
+);
 
 // @router DELETE api/profile/experience/:exp_id
 // @decx   Delete experience from profile
@@ -226,7 +249,9 @@ router.delete(
       .then(profile => {
         // Get reomove index
         console.log(profile.experience);
-        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
 
         // Splice out of array
         profile.experience.splice(removeIndex, 1);
@@ -250,7 +275,9 @@ router.delete(
       .then(profile => {
         // Get reomove index
         console.log(profile.education);
-        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
 
         // Splice out of array
         profile.education.splice(removeIndex, 1);
@@ -266,10 +293,16 @@ router.delete(
 // @decx   Delete user and profile
 // @access Privet
 
-router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Profile.findOneAndRemove({ user: req.user.id }).then(() => {
-    User.findOneAndRemove({ _id: req.user.id }).then(() => res.json({ success: true }));
-  });
-});
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
 
 module.exports = router;

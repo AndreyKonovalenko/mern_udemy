290ff3f1cec15958eb689d118ddbf7f39ab5825e
AndreyKonovalenko <andreinew@mail.ru>
2019-01-10 21:24:43 +0300

    Added ProfileAbout component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -19,35 +19,33 @@ class Profile extends Component {
   render() {
     const { profile, loading } = this.props.profile;
     let profileContent;
-    
-    if(profile === null || loading) {
+
+    if (profile === null || loading) {
       profileContent = <Spinner />;
     } else {
       profileContent = (
         <div>
           <div className='row'>
             <div className='col-md-6'>
-              <Link to='/profiles' className="btn btn-light md-3 float-left">
+              <Link to='/profiles' className='btn btn-light md-3 float-left'>
                 Back To Profiles
               </Link>
             </div>
-            <div className='col-md-6'/>
+            <div className='col-md-6' />
           </div>
-          <ProfileHeader profile={profile}/>
-          <ProfileAbout />
-          <ProfileCreds />
-          <ProfileGitHub />
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds profile={profile} />
+          <ProfileGitHub profile={profile} />
         </div>
       );
     }
-    
+
     return (
       <div className='profile'>
         <div className='container'>
           <div className='row'>
-            <div className='col-md-12'>
-              {profileContent}
-            </div>
+            <div className='col-md-12'>{profileContent}</div>
           </div>
         </div>
       </div>
--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import isEmpty from '../../validation/is-empty';
 
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+
+    // Get first name
+    const firstName = profile.user.name.trim().split(' ')[0];
+
+    // Skills List
+    let skillsData;
+
+    if (profile.skills) {
+      skillsData = profile.skills.map((element, index) => (
+        <div className='p-3' key={index}>
+          <i className='fa fa-check' /> {element}
+        </div>
+      ));
+    }
+
     return (
-    <div>
-      <h1>
-        TODO: PROFILE ABOUT
-      </h1>
-    </div>
+      <div className='row'>
+        <div className='col-md-12'>
+          <div className='card card-body bg-light mb-3'>
+            <h3 className='text-center text-info'>{firstName}'s Bio</h3>
+            <p className='lead'>
+              {isEmpty(profile.bio) ? (
+                <span>{firstName} does not have a bio</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
+            </p>
+            <hr />
+            <h3 className='text-center text-info'>Skill Set</h3>
+            <div className='row'>
+              <div className='d-flex flex-wrap justify-content-center align-items-center'>
+                {skillsData}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
 
 export default ProfileAbout;

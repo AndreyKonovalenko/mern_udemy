2991a5d177c0585005458688f787d266564aa2f6
AndreyKonovalenko <andreinew@mail.ru>
2019-01-12 19:56:53 +0300

    Finnished ProfileGitHub component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -35,8 +35,13 @@ class Profile extends Component {
           </div>
           <ProfileHeader profile={profile} />
           <ProfileAbout profile={profile} />
-          <ProfileCreds education={profile.education} experience={profile.experience} />
-          {profile.githubusername ? (<ProfileGitHub username={profile.githubusername} />) : null}
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          {profile.githubusername ? (
+            <ProfileGitHub username={profile.githubusername} />
+          ) : null}
         </div>
       );
     }

1673b1c0d2a6eac45b1e5c46ab74303c4e1e7176
Indrandeva <andreinew@mail.ru>
2019-01-15 10:34:08 +0000

    Created PostFeed component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import PostForm from './PostForm';
-//import Spinner from '../common/Spinner';
+import Spinner from '../common/Spinner';
+import PostFeed from './PostFeed';
+
+import {getPosts} from '../../actions/postActions';
+
 
 class Posts extends Component {
+  
+  componentDidMount() {
+    this.props.getPosts();
+  }
+  
   render() {
+    const {posts, loading}= this.props.post;
+    let postContent;
+    
+    if(posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = < PostFeed posts={posts} />;
+    }
+    
     return (
       <div className='feed'>
         <div className='container'>
           <div className='col-md-12'>
             <PostForm />
+            <postContent />
           </div>
         </div>
       </div>
@@ -18,4 +37,13 @@ class Posts extends Component {
   }
 }
 
-export default connect()(Posts);
+Posts.propTypes = {
+  post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(mapStateToProps, {getPosts})(Posts);

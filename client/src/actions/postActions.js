ad648656d6bee4367e769120f5becc02479767d2
Indrandeva <andreinew@mail.ru>
2019-01-15 10:05:57 +0000

    Created GET_POSTS action and reducer for it

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from '../axios-db';
 
-import { ADD_POST, GET_ERRORS } from './types';
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -19,3 +19,29 @@ export const addPost = postData => dispatch => {
       })
     );
 };
+
+// Get Posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get('api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS, // we don't call GET_ERRORS because there is no form validation where we need specific errors
+        payload: null
+      })
+    );
+};
+
+// Set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  }
+}
--- client/src/reducers/postReduxcer.js
+++ client/src/reducers/postReduxcer.js
@@ -1,5 +1,7 @@
 import {
-  ADD_POST
+  ADD_POST,
+  GET_POSTS,
+  POST_LOADING
 } from '../actions/types';
 
 
@@ -11,11 +13,22 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case POST_LOADING: 
+      return {
+        ...state,
+        loading: true
+      };
     case ADD_POST: 
       return {
         ...state,
         posts: [action.payload, ...state.posts]
       };
+    case GET_POSTS: 
+        return {
+          ...state,
+          posts: action.payload,
+          loading: false
+        };
     default:
       return state;
   }

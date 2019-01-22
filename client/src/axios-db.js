2b6e241c5db71ff37f61537a14fd343271fc8d01
Indrandeva <andreinew@mail.ru>
2019-01-22 13:51:09 +0000

    updated axios-db.js file with new url

--- client/src/axios-db.js
+++ client/src/axios-db.js
@@ -3,10 +3,15 @@ import axios from 'axios';
 //const url = 'http://localhost:5000'; // this is for local development
 //const url = 'http://mern-bereon.c9users.io:8081'; // this is for C9 IDE development
 
-let url = 'http://localhost:5000';
+let url = 'https://udemy-mern-devconnector.herokuapp.com/';
+console.log(process.env.REACT_APP_URL);
+console.log(process.NODE_ENV);
 
 if(process.env.REACT_APP_URL === 'http://mern-bereon.c9users.io:8081') {
     url = process.env.REACT_APP_URL;
+} else if (process.env.REACT_APP_URL === 'http://localhost:3000') {
+  url = 'http://localhost:5000';
+
 }
 
 const instance = axios.create({

93a263265d634178618e8fba8b5fb45673b197cf
Indrandeva <andreinew@mail.ru>
2019-01-22 14:09:35 +0000

    Final commit

--- client/src/axios-db.js
+++ client/src/axios-db.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
+
+// Production on Heroku
+
+let url = 'https://udemy-mern-devconnector.herokuapp.com/';  
+
+// Development Mode 
 //const url = 'http://localhost:5000'; // this is for local development
 //const url = 'http://mern-bereon.c9users.io:8081'; // this is for C9 IDE development
 
-let url = 'https://udemy-mern-devconnector.herokuapp.com/';  // this is only for production 
-console.log(process.env.REACT_APP_URL);
-console.log(process.NODE_ENV);
-
-if(process.env.REACT_APP_URL === 'http://mern-bereon.c9users.io:8081') {
-    url = process.env.REACT_APP_URL;
-} else if (process.env.REACT_APP_URL === 'http://localhost:3000') {
-  url = 'http://localhost:5000';
-
+if (process.env.REACT_APP_URL) {
+  console.log(process.env.REACT_APP_URL);
+  if (process.env.REACT_APP_URL === 'http://mern-bereon.c9users.io:8081') {
+      url = process.env.REACT_APP_URL;
+  } else {
+    url = 'http://localhost:5000';
+  }
 }
 
 const instance = axios.create({

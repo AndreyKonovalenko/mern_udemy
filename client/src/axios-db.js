db07adb874be48130ef4756bb049c9a1b22566ea
AndreyKonovalenko <andreinew@mail.ru>
2019-01-23 18:48:46 +0300

    Updated axios-db.js

--- client/src/axios-db.js
+++ client/src/axios-db.js
@@ -1,26 +1,19 @@
 import axios from 'axios';
 
-
 // Production on Heroku
 
-let url = 'https://udemy-mern-devconnector.herokuapp.com/';  
+let url = 'https://udemy-mern-devconnector.herokuapp.com/';
 
-// Development Mode 
+// Development Mode
 //const url = 'http://localhost:5000'; // this is for local development
 //const url = 'http://mern-bereon.c9users.io:8081'; // this is for C9 IDE development
 
-if (process.env.REACT_APP_URL) {
-  console.log(process.env.REACT_APP_URL);
-  if (process.env.REACT_APP_URL === 'http://mern-bereon.c9users.io:8081') {
-      url = process.env.REACT_APP_URL;
-  } else {
-    url = 'http://localhost:5000';
-  }
+if (process.env.REACT_APP_URL === 'http://mern-bereon.c9users.io:8081') {
+  url = process.env.REACT_APP_URL;
 }
 
 const instance = axios.create({
   baseURL: url
-  
 });
 
 export default instance;
